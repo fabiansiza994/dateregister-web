680b628a042dbb1adf6f24d0e4d1c8cfaab53788
fabiansiza994 <fabiansiza994@gmail.com>
2025-10-03 20:43:06 -0500

    modulos para apk y ipa

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fmsp.dataregisterapp',
+  appName: 'dataregisterapp',
+  webDir: 'dist/dataregister-web/browser',
+};
+
+export default config;
